3b7103c0b6193fc64c5e4f73408b739fa1eaeb00
KalimGithub <sk22kalim@gmail.com>
2023-12-15 23:09:21 +0530

    first commit

---
+++ src/Components/Main.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div className='main'>
+          <input 
+            type="text" 
+            placeholder="Search" 
+          />
+        <button id='submit-btn'>Search</button>
+    </div>
+  )
+}
+
+export default Main
