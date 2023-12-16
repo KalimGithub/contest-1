60a3fef2d035bca0c06102f0f2f8ac30142b41ee
KalimGithub <sk22kalim@gmail.com>
2023-12-17 00:27:42 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import Header from "./Components/Header";
+
 import Main from './Components/Main';
 function App() {
   return (
     <div className="App">
-     <Header />
      <Main />
     </div>
   );
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,21 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Main = () => {
 
-    function handleSearch(e) {
-        e.preventDefault()
-        console.log('Searching...');
-    }
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
 
-
+  function handleSearch(e) {
+    e.preventDefault()
+    password === confirmPassword && password != '' ?
+      alert('Form submitted successfully!') : alert('Form cannot be submitted!!');
+      setEmail('')
+      setPassword('')
+      setConfirmPassword('')
+  }
 
   return (
+
     <div className='main'>
-          <input 
-            type="text" 
-            placeholder="Search" 
-          />
-        <button id='submit-btn' onClick={handleSearch}>Search</button>
+      <h2 style={{textAlign:'center'}}>Welcome!!!</h2>
+      <label className='label'>Email:</label>
+      <input
+        id='email'
+        type='email'
+        placeholder="Enter your Email"
+        required
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <label className='label'>Password:</label>
+      <input
+        id='passowrd'
+        type='password'
+        placeholder='Enter your password'
+        required
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      {
+        password.length < 8 && password.length > 0 && <p style={{ color: 'red' }}>password must be 8 characters long</p>
+      }
+
+      <label className='label'>Confirm Password:</label>
+      <input
+        id='confirm-passowrd'
+        type='password'
+        placeholder='Enter your confirm password'
+        required
+        value={confirmPassword}
+        onChange={(e) => setConfirmPassword(e.target.value)}
+      />
+      {password !== confirmPassword && confirmPassword.length > 0 && <p style={{ color: 'red' }}>password does not match!</p>}
+
+      <button id='btn' onClick={handleSearch}>
+        Sign Up
+      </button>
     </div>
   )
 }
